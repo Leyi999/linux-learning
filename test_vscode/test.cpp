6545ef309a343da3269bed460b4b9d859027fd0b
leyi999 <1779579409@qq.com>
2022-10-30 16:36:56 +0800

    学习使用vscode远程开发

---
+++ test_vscode/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "writen in windows" << endl;
+    cout << "writen in windows" << endl;
+    cout << "writen in windows" << endl;
+    cout << "writen in vim" << endl;
+    cout << "writen in vim" << endl;
+    cout << "writen in vim" << endl;
+    return 0;
+}
